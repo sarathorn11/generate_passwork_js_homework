2dd5460aa7886866ae1e1c51dd2b17b63cad7433
sarathorn11 <raths8546@gmail.com>
2024-03-02 17:11:21 +0700

    generate passwork by js

---
+++ js/mail.js
@@ -0,0 +1,34 @@
+function generatePassword() {
+    var length = document.getElementById('length').value;
+    var uppercase = document.getElementById('uppercase').checked;
+    var lowercase = document.getElementById('lowercase').checked;
+    var numbers = document.getElementById('numbers').checked;
+    var symbols = document.getElementById('symbols').checked;
+  
+    var charset = '';
+    if (uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (numbers) charset += '0123456789';
+    if (symbols) charset += '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    
+    var password = '';
+    for (var i = 0; i < length; i++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+  
+    document.getElementById('password').textContent = password;
+  }
+  
+  function copyToClipboard() {
+    var textarea = document.createElement('textarea');
+    var password = document.getElementById('password').textContent;
+    if (!password) return;
+    
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert('Password copied to clipboard!');
+  }
+  
